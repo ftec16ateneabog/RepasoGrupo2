7dd2170c869bdd6f78fdefa0e89c3e5e2d03185e
ftec16ateneabog <ftec16atenea_bog@unal.edu.co>
2023-12-12 19:30:36 -0500

    first commit

--- app/src/main/java/com/atenea/repaso/MainActivity.java
+++ app/src/main/java/com/atenea/repaso/MainActivity.java
@@ -3,9 +3,14 @@ package com.atenea.repaso;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.DatePicker;
 import android.widget.TableLayout;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.android.material.textview.MaterialTextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,11 +20,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatePicker dateCumple = findViewById(R.id.fecha);
-        MaterialTextView materialTextViewName = findViewById(R.id.NombreTextView);
+        //DatePicker dateCumple = findViewById(R.id.fecha);
+        TextInputEditText materialTextViewName = findViewById(R.id.nombreEditText);
+        TextInputEditText mailTextViewName = findViewById(R.id.MailEditText);
         //int fecha = dateCumple.getDayOfMonth();
 
-        Security.validarSize(materialTextViewName.getText().toString(),6,12);
+        materialTextViewName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(Security.validarSize(charSequence.toString(),6,12))
+                {
+                    Toast.makeText(MainActivity.this, "El nombre cumple con el tamano", Toast.LENGTH_LONG).show();
+                }else{
+                    materialTextViewName.setError("el texto no cumple con las politicas de seguridad");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mailTextViewName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+               boolean a = Security.validarMail(charSequence.toString());
+               if(a)
+               {
+                 Toast.makeText(MainActivity.this,"validacion pattern true",Toast.LENGTH_SHORT).show();
+               }else {
+                   Toast.makeText(MainActivity.this,"validacion pattern false",Toast.LENGTH_SHORT).show();
+               }
+               boolean b = Security.validarTexto(charSequence.toString());
+                if(b)
+                {
+                    Toast.makeText(MainActivity.this,"validacion manual true",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this,"validacion manual false",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
     }
 
 
--- app/src/main/java/com/atenea/repaso/Security.java
+++ app/src/main/java/com/atenea/repaso/Security.java
@@ -1,8 +1,23 @@
 package com.atenea.repaso;
 
+import android.util.Patterns;
+
 public class Security {
     public static boolean validarSize(String textoAValidar, int min, int max)
     {
         return textoAValidar.length() >= min && textoAValidar.length() <= max;
     }
+    public static boolean validarTelefono(String tel){
+        return Patterns.PHONE.matcher(tel).matches();
+    }
+
+    public static boolean validarMail(String tel){
+        return Patterns.EMAIL_ADDRESS.matcher(tel).matches();
+    }
+
+    public static boolean validarTexto(String texto)
+    {
+        return texto.matches("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+    }
+
 }
